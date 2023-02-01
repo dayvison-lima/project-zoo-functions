1be891e0f4767e26206e5f170be74ed9202df7ae
Dayvison Lima <limaare@outlook.com>
2023-02-01 14:15:37 -0300

    commit inicial, testando requisitos countAnimais, getAnimalsOlderThan, getEmployeeByName e getSpeciesByIds

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const objAnimais = data.species.reduce((acc, specie) => {
+  acc[specie.name] = specie.residents.length;
+  return acc;
+}, {});
+
+console.log(objAnimais);
+
 const countAnimals = (animal) => {
   // seu código aqui
+  if (animal === undefined) return objAnimais;
 };
 
 module.exports = countAnimals;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
+const getSpeciesByIds = (...ids) => {
   // seu código aqui
+
+  const result = data.species.filter((specie) => ids.some((id) => specie.id === id));
+  if (ids === undefined) {
+    return [];
+  }
+  return result;
 };
 
 module.exports = getSpeciesByIds;
