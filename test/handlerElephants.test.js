92c60b68892e8bf13a016c503de126f5cfc4a69d
Dayvison Lima <limaare@outlook.com>
2023-02-02 15:01:12 -0300

    resolvido requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('verifica se a função handlerElephants tem o comportamento esperado', () => {
+    // testa se é uma função
+    expect(typeof handlerElephants).toBe('function');
+    // testa se retorna undefined quando o param é undefined
+    expect(handlerElephants()).toBeUndefined();
+    // testa se o param é uma string
+    const notString = 'Parâmetro inválido, é necessário uma string';
+    expect(handlerElephants(123)).toEqual(notString);
+  });
+});
