e36d618b2f53c4f0af92f86d66446ce3024fd088
Dayvison Lima <limaare@outlook.com>
2023-02-02 17:25:20 -0300

    resolvido requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,24 @@ const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
   // seu código aqui
+  const quantityChild = entrants.filter((person) => person.age < 18).length;
+  const quantityAdult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const quantitySenior = entrants.filter((person) => person.age >= 50).length;
+  return {
+    child: quantityChild,
+    adult: quantityAdult,
+    senior: quantitySenior,
+  };
 };
 
 const calculateEntry = (entrants) => {
   // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const visitantes = countEntrants(entrants);
+  const valorChild = visitantes.child * data.prices.child;
+  const valorAdult = visitantes.adult * data.prices.adult;
+  const valorSenior = visitantes.senior * data.prices.senior;
+  return valorChild + valorAdult + valorSenior;
 };
 
 module.exports = { calculateEntry, countEntrants };
