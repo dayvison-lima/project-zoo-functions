df0a4d76cd26e9e24bc66924bb52dcbf9536a676
Dayvison Lima <limaare@outlook.com>
2023-02-02 14:39:05 -0300

    resolvido requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,15 +1,27 @@
 const data = require('../data/zoo_data');
 
-const objAnimais = data.species.reduce((acc, specie) => {
-  acc[specie.name] = specie.residents.length;
-  return acc;
-}, {});
+const animalsBySex = (animal) => {
+  const animals = data.species.find((spec) => spec.name === animal.species).residents;
+  const animalSex = animals
+    .reduce((acc, curr) => ((curr.sex === animal.sex) ? (acc + 1) : (acc)), 0);
+  return animalSex;
+};
 
-console.log(objAnimais);
+const animalsWithoutSex = (animal) => (data.species
+  .find((spec) => spec.name === animal.species).residents.length);
 
 const countAnimals = (animal) => {
   // seu código aqui
-  if (animal === undefined) return objAnimais;
+  if (!animal) {
+    return data.species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  if (Object.keys(animal).length === 1) {
+    return animalsWithoutSex(animal);
+  }
+  return animalsBySex(animal);
 };
 
 module.exports = countAnimals;
